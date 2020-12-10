5fce7b2d451cc84481f75aeaf48e36ed83f7ab6b
ROCIO ARRABALIN SERRA <rocio_as90@hotmail.com>
2020-12-10 20:07:16 +0100

    Practice 1

---
+++ js/practica1.js
@@ -0,0 +1,3 @@
+document.getElementById("name").value = "ROCIO";
+document.getElementById("lastname").value = "ARRABALIN";
+document.getElementById("avatar").src = "https://www.w3schools.com/howto/img_avatar.png";
